167d7c4074b33e27d9bb50a7d4b07aae7b91b4b3
Clarkdale <clarkdale@email.arizona.edu>
2017-12-12 10:43:06 -0700

    Computer is now generating random moves

--- Computer.java
+++ Computer.java
@@ -29,13 +29,24 @@ public class Computer {
 
     Piece chosen = selection.get(generator.nextInt(selection.size()));
 
+    int x = chosen.getColumn();
+    int y = chosen.getRow();
+
     List<Tuple> moves = new ArrayList<>(possible.get(chosen));
 
     Tuple picked = moves.get(generator.nextInt(moves.size()));
 
-    board[chosen.getRow()][chosen.getColumn()] = null;
+    while (picked.getFirst() == x && picked.getSecond() == y) {
+      chosen = selection.get(generator.nextInt(selection.size()));
+      x = chosen.getColumn();
+      y = chosen.getRow();
+      moves = new ArrayList<>(possible.get(chosen));
+
+      picked = moves.get(generator.nextInt(moves.size()));
+    }
     chosen.setX(picked.getFirst());
     chosen.setY(picked.getSecond());
+    board[y][x] = null;
     board[chosen.getRow()][chosen.getColumn()] = chosen;
   }
 
