f79c1ea0503b20ab0253325b2213ab2fc8204545
Clarkdale <clarkdale@email.arizona.edu>
2017-12-23 16:52:17 -0700

    Comments added

--- King.java
+++ King.java
@@ -32,59 +32,80 @@ public class King extends Piece {
     } //end if/else
   } //end method
 
+  /*====================================================================
+     Method Name:  move
+         Purpose:  Creates a set of tuples, which hold (x.y) positions
+                   on the board as to where this type of piece could
+                   potentially move. Essentially a "scan board" method
+      Parameters:  in: A 2D array of pieces, which represent the board
+         Returns:  A set of tuples of all possible moves
+  ====================================================================*/
   public Set<Tuple> move(Piece [][] in) {
+    //current variables set to compare back to in later logic
     int currX = super.getColumn();
     int currY = super.getRow();
+
+    //Set of possile moves of tuples initialized
     Set<Tuple> out = new HashSet<>();
+
+    //adds original position to set, original position is considered a "Invalid
+    //move"
     out.add(new Tuple(currX, currY));
 
+    //logic for all moves one square to the left of the current king loccation,
+    //in relation to the initial position.
     if (currX - 1 >= 0) {
       if (currY - 1 >= 0) {
         if (in[currY - 1][currX - 1] == null || in[currY - 1][currX - 1].type() != super.type()) {
           out.add(new Tuple(currX - 1, currY - 1));
-        }
-      }
+        } //end if
+      } //end if
 
       if (currY + 1 <= 7) {
         if (in[currY + 1][currX - 1] == null || in[currY + 1][currX - 1].type() != super.type()) {
           out.add(new Tuple(currX - 1, currY + 1));
-        }
-      }
+        } //end if
+      } //end if
 
       if (in[currY][currX - 1] == null || in[currY][currX - 1].type() != super.type()) {
         out.add(new Tuple(currX - 1, currY));
-      }
-    }
+      } //end if
+    } //end if
 
+    //handles logic for all potential moves to the right of the initial king
+    //posiiton
     if (currX + 1 <= 7) {
       if (currY - 1 >= 0) {
         if (in[currY - 1][currX + 1] == null || in[currY - 1][currX + 1].type() != super.type()) {
           out.add(new Tuple(currX + 1, currY - 1));
-        }
-      }
+        } //end if
+      } //end if
 
       if (currY + 1 <= 7) {
         if (in[currY + 1][currX + 1] == null || in[currY + 1][currX + 1].type() != super.type()) {
           out.add(new Tuple(currX + 1, currY + 1));
-        }
-      }
+        } //end if
+      } //end if
 
       if (in[currY][currX + 1] == null || in[currY][currX + 1].type() != super.type()) {
         out.add(new Tuple(currX + 1, currY));
-      }
-    }
+      } //end if
+    } //end if
 
+    //logic for handling above/below the initial king position
     if (currY - 1 >= 0) {
       if (in[currY - 1][currX] == null || in[currY - 1][currX].type() != super.type()) {
         out.add(new Tuple(currX, currY - 1));
-      }
-    }
+      } //end if
+    } //end if
 
+    //logic for above.below moves in relation to the initial king position
     if (currY + 1 <= 7) {
       if (in[currY + 1][currX] == null || in[currY + 1][currX].type() != super.type()) {
         out.add(new Tuple(currX, currY + 1));
-      }
-    }
+      } //end if
+    } //end if
+
     return out;
   } //end method
 } //end class
