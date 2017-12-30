470b89135812c1dd4be56e9c053f2d84f8362ff6
Clarkdale <clarkdale@email.arizona.edu>
2017-12-30 15:06:39 -0700

    AI is functional

--- Computer.java
+++ Computer.java
@@ -59,6 +59,106 @@ public class Computer {
     int max = -9999;
     Piece picked = null;
     Tuple location = null;
+
+    for (Piece check : pieces) {
+      Piece [][] outsideCopy = board.clone();
+      Set<Tuple> moves = check.move(outsideCopy);
+
+      for (Tuple mind : moves) {
+        int x = check.getColumn();
+        int y = check.getRow();
+
+        outsideCopy[y][x] = null;
+
+        outsideCopy[mind.getSecond()][mind.getFirst()] = check;
+
+        int num = minimax(0, 2, outsideCopy, true);
+
+        if (num > max) {
+          max = num;
+          picked = check;
+          location = mind;
+        }
+      }
+    }
+
+    board[picked.getRow()][picked.getColumn()] = null;
+    picked.setX(location.getFirst());
+    picked.setY(location.getSecond());
+    board[location.getSecond()][location.getFirst()] = picked;
+  }
+
+  public int minimax(int depth, int maxDepth, Piece [][] game, boolean turn) {
+    int max = 0;
+
+    Piece [][] copy = game.clone();
+
+
+    Set<Piece> whiteSide = new HashSet<>();
+    Set<Piece> blackSide = new HashSet<>();
+
+    for (Piece [] rows : game) {
+      for (Piece individ : rows) {
+        if (individ != null && individ.type()) {
+          whiteSide.add(individ);
+        } else if (individ != null && !individ.type()) {
+          blackSide.add(individ);
+        }
+      }
+    }
+
+    if (depth == maxDepth) {
+      for (Piece last : blackSide) {
+        Set<Tuple> moves = last.move(game);
+        for (Tuple move : moves) {
+          int checked = checkSquare(move, game);
+          if (checked > max) {
+            max = checked;
+          }
+        }
+      }
+
+      return max;
+    }
+
+    int caught;
+
+    int secondMax = 0;
+    if (turn) {
+      for (Piece toMove : blackSide) {
+        Set<Tuple> possible = toMove.move(game);
+
+        for (Tuple slide : possible) {
+          copy[toMove.getRow()][toMove.getColumn()] = null;
+          toMove.setX(slide.getFirst());
+          toMove.setY(slide.getSecond());
+          copy[slide.getSecond()][slide.getFirst()] = toMove;
+          caught = minimax(depth, maxDepth, copy, !turn);
+          if (caught > secondMax) {
+            secondMax = caught;
+          }
+        }
+      }
+      return max + secondMax;
+
+    } else {
+      for (Piece toMove : whiteSide) {
+        Set<Tuple> possible = toMove.move(game);
+
+        for (Tuple slide : possible) {
+          copy[toMove.getRow()][toMove.getColumn()] = null;
+          toMove.setX(slide.getFirst());
+          toMove.setY(slide.getSecond());
+          copy[slide.getSecond()][slide.getFirst()] = toMove;
+          caught = minimax(depth++, maxDepth, copy, !turn);
+          if (caught > secondMax) {
+            secondMax = caught;
+          }
+        }
+      }
+
+      return max - secondMax;
+    }
   }
 
 
