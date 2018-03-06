d02120fa1774256d20a7cd5c63614c5fdd5d7b7a
Clarkdale <clarkdale@email.arizona.edu>
2018-03-06 07:23:53 -0700

    King updated so it can no longer move itself into check

--- src/model/King.java
+++ src/model/King.java
@@ -22,41 +22,69 @@ public class King extends Piece {
 	public void setX(int in) {
 		if (in != super.getColumn() && in != (-super.getColumn() + 7)) {
 			moved++;
-		}
+		} //end if
 
 		super.setX(in);
-	}
+	} //end method
 
 	public void setY(int in) {
 		if (in != super.getRow() && in != (-super.getRow() + 7)) {
 			moved++;
-		}
+		} //end if
 
 		super.setY(in);
-	}
+	} //end method
 	
+	/*====================================================================
+    Method Name:  inCheck
+        Purpose:  Returns a boolean value concerning whether the king
+                  is in check
+     Parameters:  in: A 2D array of pieces, which represent the board
+        Returns:  boolean value concerning check status
+ 	====================================================================*/
 	public boolean inCheck(Piece [][] in) {
-		//TODO: look at all movesets of the opponents 
-		// pieces, and if a move contains the king in it, 
-		// reset the check boolean instance variable to
-		// true/false depending on the result and return
-		// said variable
+		//calls to helper function to reduce redundancy and so check method
+		// can be used in other instances as well
+		return moveCheck(in, super.getColumn(), super.getRow());
+	} //end method
+	
+	/*====================================================================
+    Method Name:  moveCheck
+        Purpose:  Takes in a board and potential x and y value for where 
+                  the king could move and returns a boolean value as to
+                  if that position is a threatened check position by 
+                  another piece
+     Parameters:  in: A 2D array of pieces, which represent the board
+                  x: column position to check
+                  y: row position to check
+        Returns:  boolean value concerning if that space is threatened
+ 	====================================================================*/
+	public boolean moveCheck(Piece [][] in, int x, int y) {
 		Set<Piece> opposite = new HashSet<>();
+		
+		//all enemy pieces are added to the opposite set
 		for (int i = 0; i < 8; i++) {
 			for (int j = 0; j < 8; j++) {
 				if (in[i][j] != null && in[i][j].type() != this.type())
 					opposite.add(in[i][j]);
-			}
-		}
+			} //end for
+		} //end for
 		
+		// for loop moves over enemy pieces, and checks their move set. If
+		// any of the tuples inside of this set match the x and y value passed
+		// in, the space is threatened. Kings are not checked to avoid stack
+		// overflow
 		for (Piece enemy : opposite) {
-			for (Tuple move : in[enemy.getRow()][enemy.getColumn()].move(in)) {
-				if (super.getColumn() == move.getFirst() && super.getRow() == move.getSecond())
-					return true;
-			}
-		}
+			if (!(enemy instanceof King)) {
+				for (Tuple move : in[enemy.getRow()][enemy.getColumn()].move(in)) {
+					if (x == move.getFirst() && y == move.getSecond())
+						return true;
+				} // end for
+			} //end if
+		} // end for
+		
 		return false;
-	}
+	} // end method
 
 	public int getRow() {
 		return super.getRow();
@@ -110,18 +138,18 @@ public class King extends Piece {
 		// in relation to the initial position.
 		if (currX - 1 >= 0) {
 			if (currY - 1 >= 0) {
-				if (in[currY - 1][currX - 1] == null || in[currY - 1][currX - 1].type() != super.type()) {
+				if ((in[currY - 1][currX - 1] == null || in[currY - 1][currX - 1].type() != super.type()) && !moveCheck(in, currX - 1, currY - 1)) {
 					out.add(new Tuple(currX - 1, currY - 1));
 				} // end if
 			} // end if
 
 			if (currY + 1 <= 7) {
-				if (in[currY + 1][currX - 1] == null || in[currY + 1][currX - 1].type() != super.type()) {
+				if ((in[currY + 1][currX - 1] == null || in[currY + 1][currX - 1].type() != super.type()) && !moveCheck(in, currX - 1, currY + 1)) {
 					out.add(new Tuple(currX - 1, currY + 1));
 				} // end if
 			} // end if
 
-			if (in[currY][currX - 1] == null || in[currY][currX - 1].type() != super.type()) {
+			if ((in[currY][currX - 1] == null || in[currY][currX - 1].type() != super.type()) && !moveCheck(in, currX - 1, currY)) {
 				out.add(new Tuple(currX - 1, currY));
 			} // end if
 		} // end if
@@ -130,13 +158,13 @@ public class King extends Piece {
 		// posiiton
 		if (currX + 1 <= 7) {
 			if (currY - 1 >= 0) {
-				if (in[currY - 1][currX + 1] == null || in[currY - 1][currX + 1].type() != super.type()) {
+				if ((in[currY - 1][currX + 1] == null || in[currY - 1][currX + 1].type() != super.type()) && !moveCheck(in, currX + 1, currY - 1)) {
 					out.add(new Tuple(currX + 1, currY - 1));
 				} // end if
 			} // end if
 
 			if (currY + 1 <= 7) {
-				if (in[currY + 1][currX + 1] == null || in[currY + 1][currX + 1].type() != super.type()) {
+				if ((in[currY + 1][currX + 1] == null || in[currY + 1][currX + 1].type() != super.type()) && !moveCheck(in, currX + 1, currY + 1)) {
 					out.add(new Tuple(currX + 1, currY + 1));
 				} // end if
 			} // end if
@@ -148,32 +176,34 @@ public class King extends Piece {
 
 		// logic for handling above/below the initial king position
 		if (currY - 1 >= 0) {
-			if (in[currY - 1][currX] == null || in[currY - 1][currX].type() != super.type()) {
+			if ((in[currY - 1][currX] == null || in[currY - 1][currX].type() != super.type()) && !moveCheck(in, currX, currY - 1)) {
 				out.add(new Tuple(currX, currY - 1));
 			} // end if
 		} // end if
 
 		// logic for above.below moves in relation to the initial king position
 		if (currY + 1 <= 7) {
-			if (in[currY + 1][currX] == null || in[currY + 1][currX].type() != super.type()) {
+			if ((in[currY + 1][currX] == null || in[currY + 1][currX].type() != super.type()) && !moveCheck(in, currX, currY + 1)) {
 				out.add(new Tuple(currX, currY + 1));
 			} // end if
 		} // end if
 
+		// boolean block allows for castling moves to be added to the overall
+		// moveset
 		if ((currY == 7 || currY == 0) && this.castle()) {
 			if (this.type()) {
-				if (in[currY][currX + 1] == null && in[currY][currX + 2] == null && (in[currY][currX + 3] instanceof Rook)) 
+				if (in[currY][currX + 1] == null && in[currY][currX + 2] == null && (in[currY][currX + 3] instanceof Rook) && !moveCheck(in, currX + 2, currY)) 
 					out.add(new Tuple(currX + 2, currY));
 				
-				if (in[currY][currX - 1] == null && in[currY][currX - 2] == null && in[currY][currX - 3] == null && (in[currY][currX - 4] instanceof Rook))
+				if (in[currY][currX - 1] == null && in[currY][currX - 2] == null && in[currY][currX - 3] == null && (in[currY][currX - 4] instanceof Rook) && !moveCheck(in, currX - 2, currY))
 					out.add(new Tuple(currX - 3, currY));
 			} else {
-				if (in[currY][currX - 1] == null && in[currY][currX - 2] == null && (in[currY][currX - 3] instanceof Rook))
+				if (in[currY][currX - 1] == null && in[currY][currX - 2] == null && (in[currY][currX - 3] instanceof Rook) && !moveCheck(in, currX - 2, currY))
 					out.add(new Tuple(currX - 2, currY));
-				if (in[currY][currX + 1] == null && in[currY][currX + 2] == null && in[currY][currX + 3] == null && (in[currY][currX + 4] instanceof Rook))
+				if (in[currY][currX + 1] == null && in[currY][currX + 2] == null && in[currY][currX + 3] == null && (in[currY][currX + 4] instanceof Rook) && !moveCheck(in, currX + 3, currY))
 					out.add(new Tuple(currX + 3, currY));
-			}
-		}
+			} //end if/else
+		} //end if
 
 		return out;
 	} // end method
